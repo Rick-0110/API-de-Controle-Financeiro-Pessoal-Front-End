cacc1beca1f49dbc92f40abcbab6eea05a548510
Henrique <matosh1604@gmail.com>
2026-01-12 22:55:25 -0300

    First commit

---
+++ js/auth.js
@@ -0,0 +1,45 @@
+const apiBaseUrl = 'https://localhost:7155/api';
+
+const loginForm = document.getElementById('loginForm');
+const errorMessage = document.getElementById('errorMessage');
+
+if(loginForm){
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault(); 
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const loginData = {
+            email: email,
+            password: password
+        };
+
+        try {
+
+            const response = await fetch(`${apiBaseUrl}/Auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginData)
+            });
+
+        
+            if (response.ok) {
+                const data = await response.text(); 
+                
+                localStorage.setItem('token', data);
+                
+                window.location.href = 'dashboard.html';
+            } else {
+                errorMessage.textContent = "Email ou senha inválidos!";
+                errorMessage.style.display = 'block';
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            errorMessage.textContent = "Erro ao conectar com o servidor.";
+            errorMessage.style.display = 'block';
+        }
+    });
+}
